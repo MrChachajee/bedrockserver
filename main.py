51623bd2cdd63b2552abc90ce0669224873a918a
= <=>
2021-12-27 10:21:54 +0500

    sd

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os, time
 
 print("STARTING...")
-os.system("host myip.opendns.com resolver1.opendns.com")
+print("Starting Web...")
+os.system('heroku-php-apache2 &')
+
+# os.system("host myip.opendns.com resolver1.opendns.com")
+
 os.system("wget -O mc.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.2.03.zip")
-print("Fetched")
 time.sleep(5)
+
 os.system("unzip mc.zip")
-print("Unzip")
+print("Unzipped")
 os.system("chmod +x *")
+
+print("Starting Server...")
+os.system("echo LD_LIBRARY_PATH=. ./bedrock_server > run.sh")
+os.system("chmod +x ./run.sh")
+os.system("nohup ./run.sh > server.out &")
 time.sleep(5)
+
+
+while True:
+    result = open('server.out', 'r').read().find('Server started.')
+    if result > -1:
+        break
+
 print("Ngrok starting... ")
 os.system('ngrok tcp -region us 25565 &')
 time.sleep(5)
-print("Starting Server...")
+
 os.system("curl ifconfig.me")
-os.system("echo LD_LIBRARY_PATH=. ./bedrock_server > run.sh")
-os.system("chmod +x ./run.sh")
-os.system("nohup ./run.sh &")
