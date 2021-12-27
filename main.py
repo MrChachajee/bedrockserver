7bbdb43181bd77b10724313f6376e99a05acae14
= <=>
2021-12-27 15:25:47 +0500

    dfv

--- main.py
+++ main.py
@@ -9,4 +9,21 @@ os.system("wget -q -O - https://raw.githubusercontent.com/pmmp/php-build-scripts
 
 time.sleep(5)
 
-os.system("./start.sh --no-wizard")
+os.system("nohup ./start.sh --no-wizard > server.out &")
+
+while True:
+    time.sleep(10)
+    result = open('nohup.out', 'r').read().find('Done')
+    if result > -1:
+        print("Started...")
+        break
+
+print("**script**: starting ngrok tcp")
+os.system('ngrok tcp -region us 19132 &')
+
+
+while True:
+    result = open('nohup.out', 'r').read()
+    print(result)
+    time.sleep(30)
+
