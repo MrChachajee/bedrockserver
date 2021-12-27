f12c8b7ba7532d18242a43ff83f51fe742a7e39b
= <=>
2021-12-27 15:16:20 +0500

    dfv

--- main.py
+++ main.py
@@ -5,34 +5,8 @@ print("Starting Web...")
 os.system('heroku-php-apache2 &')
 
 # os.system("host myip.opendns.com resolver1.opendns.com")
+os.system("wget -q -O - https://raw.githubusercontent.com/pmmp/php-build-scripts/master/installer.sh | bash -s -")
 
-os.system("wget -O mc.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.2.03.zip")
 time.sleep(5)
 
-os.system("unzip mc.zip")
-print("Unzipped")
-os.system("chmod +x *")
-
-print("Starting Server...")
-os.system("nohup ./bedrock_server > server.out &")
-time.sleep(5)
-
-
-while True:
-    result = open('server.out', 'r').read().find('Server started.')
-    if result > -1:
-        print("Started.. ")
-        break
-
-print("Ngrok starting... ")
-os.system('ngrok tcp -region us 19132 &')
-
-while True:
-    result2 = open('server.out', 'r').read()
-    result1 = open('server.out', 'r').read().find('Stopping server...')
-    print(result2)
-    os.system("curl ifconfig.me")
-    if result1 > -1:
-        print("Stopped...")
-        break
-    time.sleep(30)
+os.system("./run.sh --no-wizard")
